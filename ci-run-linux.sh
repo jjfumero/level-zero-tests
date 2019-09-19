241c68cef94d5c96a5620b6e2d757bfb5b298734
Lisanna Dettwyler <lisanna.dettwyler@intel.com>
2019-09-19 13:43:08 -0700

    Fix runtime artifact paths (again)

--- ci-run-linux.sh
+++ ci-run-linux.sh
@@ -3,7 +3,8 @@
 set -eu
 set -o pipefail
 
-test=$1
+test_dir=$1
+test=$2
 
 results_dir=$PWD/run_results
 mkdir -p $results_dir
@@ -16,7 +17,7 @@ echo "running $test" &>> run.log
 export LD_LIBRARY_PATH=$PWD/third_party/level_zero_linux/lib/level_zero
 export SetCommandStreamReceiver=2
 export ProductFamilyOverride=skl
-cd out/conformance_tests
+cd $test_dir
 ./$test --logging-level=error | tee $results_dir/$test || true  # why isn't --logging-level=error the default?!
 touch $results_dir/$test-done
 ) &
