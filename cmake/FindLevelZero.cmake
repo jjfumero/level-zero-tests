d1bcf3f87b4a75bb92a3bde7e67a3e0ef21e1e59
Lockett, Jemale <jemale.lockett@intel.com>
2019-11-18 09:53:29 -0800

    Use dynamic loader ze_loader.so instead of level_zero.so

--- cmake/FindLevelZero.cmake
+++ cmake/FindLevelZero.cmake
@@ -18,7 +18,7 @@ if (OpenCL_FOUND)
 endif()
 
 find_library(LevelZero_LIBRARY
-  NAMES level_zero level_zero32 level_zero64
+  NAMES ze_loader ze_loader32 ze_loader64
   PATHS
     ${L0_ROOT}
     "/usr/local/"
@@ -35,7 +35,7 @@ find_package_handle_standard_args(LevelZero
 mark_as_advanced(LevelZero_LIBRARY LevelZero_INCLUDE_DIR)
 
 if(LevelZero_FOUND)
-    list(APPEND LevelZero_LIBRARIES ${LevelZero_LIBRARY})
+    list(APPEND LevelZero_LIBRARIES ${LevelZero_LIBRARY} ${CMAKE_DL_LIBS})
     list(APPEND LevelZero_INCLUDE_DIRS ${LevelZero_INCLUDE_DIR})
     if(OpenCL_FOUND)
         list(APPEND LevelZero_INCLUDE_DIRS ${OpenCL_INCLUDE_DIRS})
